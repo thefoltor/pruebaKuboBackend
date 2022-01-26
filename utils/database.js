328a48d02be95f9b2140965952750798754f2f3f
David Martin <thefoltor@hotmail.com>
2022-01-26 15:26:53 -0500

    make it better

--- utils/database.js
+++ utils/database.js
@@ -4,10 +4,11 @@ const sequelize = new Sequelize("dcnoudk8vmujtp",
 "kxnkjxwxdhwnfe", 
 "03efbb0af42a18611736d0c38467d2ae54dc4911233c0249bda8ea1b096c6a64", {
     dialect: "postgres", 
+    protocol: "postgres",
+    logging:true,
     host: "ec2-3-224-251-47.compute-1.amazonaws.com",
     dialectOptions: {
         ssl:{
-            require:true,
             rejectUnauthorized: false
         }
       }
