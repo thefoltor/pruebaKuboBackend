72e3cbbf537c1fe84e42aef98177e88aa19df612
David Martin <thefoltor@hotmail.com>
2022-01-26 14:55:02 -0500

    make it better

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("pruebaKubo", "root", "", {
-    dialect: "mysql", 
-    host: "localhost",
+const sequelize = new Sequelize("dcnoudk8vmujtp", 
+"kxnkjxwxdhwnfe", 
+"03efbb0af42a18611736d0c38467d2ae54dc4911233c0249bda8ea1b096c6a64", {
+    dialect: "postgres", 
+    host: "ec2-3-224-251-47.compute-1.amazonaws.com",
 } );
 
 module.exports = sequelize;
